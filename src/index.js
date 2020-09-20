2ce0df2fe62652bf54bfa5b1727bd2f9cca2bcba
Ilya Grachev <ilya095@gmail.com>
2020-09-20 17:15:10 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+// import './module';
+// import './scss/index.scss';
+
+// import 'bootstrap';
+// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/dist/dropdown'
+import './scss/app.scss';
+
+console.log('test!');
